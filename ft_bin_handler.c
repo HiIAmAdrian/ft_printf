801cc3527bb47e14807655aaa145e4cd825e80d5
hiiamadrian <adstan@student.42.ft>
2018-04-14 11:24:03 +0300

    added binary

--- ft_bin_handler.c
+++ ft_bin_handler.c
@@ -6,7 +6,7 @@
 /*   By: adstan <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/04/13 20:19:59 by adstan            #+#    #+#             */
-/*   Updated: 2018/04/13 20:22:23 by adstan           ###   ########.fr       */
+/*   Updated: 2018/04/14 11:23:47 by adrian           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,4 +16,14 @@ int             one_for_all(char *str, t_format args, char *pre);
 
 int		ft_bin_handler(va_list *list, t_format args)
 {
-	uintmax_t
+      	uintmax_t n;
+	char	    *str;
+	int	    ret;
+
+	n = lenght_uint(list, &args);
+	str = ft_strdup(ft_itoa_ulong(n, 2, 0));
+	if (args.precision != -1)
+	  args.zero = 0;
+	ret = one_for_all(str, args, "");
+	return (ret);
+}
